10ade83f6b1e425c026f60c57764bc69b91f2940
TrueDi1905 <75692134+TrueDi1905@users.noreply.github.com>
2022-03-24 18:30:56 +0300

    Добавил примечание

--- tg_bot.py
+++ tg_bot.py
@@ -1,5 +1,7 @@
 from telegram import Bot
 
+#данные указаны напрямую, без сокрытия, для удобства тестирования
+#в любом другом случае прячем данные в .env
 CHAT_ID = "672595973"
 TELEGRAM_TOKEN = "5236192500:AAHRP8YYqc9cBSto4yFehUPJIsUoSdIdwqs"
 
